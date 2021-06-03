2752ed3648abbf2af021864773cffbb1b0df9b3e
Chinadean <20118444+Chinadean@users.noreply.github.com>
2021-06-03 14:58:18 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HelloWiredElements />
+  </div>
+</template>
+
+<script>
+import HelloWiredElements from "./components/HelloWiredElements";
+
+export default {
+  name: "App",
+  components: {
+    HelloWiredElements,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Gloria Hallelujah", cursive;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  font-size: 16px;
+  line-height: 1.8;
+}
+</style>
